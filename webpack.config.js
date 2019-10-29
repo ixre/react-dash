a99226be3aed83d3e7236e049b1d751f68eaa781
jarrysix <jarrysix@gmail.com>
2019-10-29 16:22:31 +0800

    package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,46 @@
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
+    output: {
+        filename: 'bundle_[name]_[hash].js',
+        publicPath: "/",
+    },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
                 }
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true,
+                        },
+                    },
+                    "css-loader"]
             }
         ]
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        //new BundleAnalyzerPlugin(),
         new HtmlWebPackPlugin({
             template: "./public/index.html",
             filename: "./index.html"
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: `styles/[name].css`
+        }),
     ]
 };
